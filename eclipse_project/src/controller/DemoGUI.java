1c30c3f8cb6293459bdb2db737a8bfb38d1c3177
OzGhost <xad.love@gmail.com>
2017-05-02 00:55:04 +0700

    Obserable-Observer exam + JTattoo skin

--- eclipse_project/src/controller/DemoGUI.java
+++ eclipse_project/src/controller/DemoGUI.java
@@ -1,18 +1,25 @@
 package controller;
 
-import db.KhachHang;
-import db.db;
+import javax.swing.UIManager;
+
+import model.DemoModel;
+import view.DemoView;
 
 public class DemoGUI {
     public static void main (String[] args) {
-//        final DemoView view = new DemoView();
-//        final DemoModel model = new DemoModel();
-//        model.addObserver(view);
-//        final DemoController controller = new DemoController(view, model);
-//        view.addController(controller);
-//        view.setVisible(true);
-        db.init("orcBase", "c##oz", "ngaymai");
-        KhachHang.init();
-        KhachHang.findOne();
+        try {
+            UIManager.setLookAndFeel("com.jtattoo.plaf.hifi.HiFiLookAndFeel");
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        final DemoView view = new DemoView();
+        final DemoModel model = new DemoModel();
+        model.addObserver(view);
+        final DemoController controller = new DemoController(view, model);
+        view.addController(controller);
+        view.setVisible(true);
+//        db.init("orcBase", "c##oz", "ngaymai");
+//        KhachHang.init();
+//        KhachHang.findOne();
     }
 }
